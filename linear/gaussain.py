6f133389d02dd8b5ca28faf638506c18c838ea67
Shreykc1 <44289344+Shreykc1@users.noreply.github.com>
2024-04-08 01:06:15 +0530

    Add files via upload

---
+++ linear/gaussain.py
@@ -0,0 +1,34 @@
+
+def gaussain(m):
+    
+    rows = len(m)
+    cols = len(m[0])
+    
+    # for getting bottom left 0's
+    for i in range(rows):
+        if m[i][i] == 0.0:
+            return "Invalid"
+        for j in range(rows):
+            if i!=j:
+                factor = m[j][i] / m[i][i]
+                for k in range(cols):
+                    m[j][k] = m[j][k] - factor * m[i][k]
+    [print(row) for row in m]
+    print()
+    
+
+    
+    # for getting slant 1's
+    for i in range(rows):
+        factor = m[i][i]
+        for j in range(cols):
+            m[i][j] = round(m[i][j] / factor)
+    [print(row) for row in m]
+    
+    
+                    
+                
+    
+
+m = [[3.0, 2.0, -4.0, 3.0], [2.0, 3.0, 3.0, 15.0], [5.0, -3, 1.0, 14.0]]
+print(gaussain(m))
